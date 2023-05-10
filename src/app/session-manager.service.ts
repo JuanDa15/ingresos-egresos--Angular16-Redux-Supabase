73cded1cbdb60181855f1c81f37be9969c1e948e
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-05-09 22:52:40 -0500

    create servise to manage the session

---
+++ src/app/session-manager.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, inject } from '@angular/core';
+import { SupabaseService } from './supabase.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionManagerService {
+
+  public db = inject(SupabaseService);
+
+  public async getUser() {
+    const session = await this.getSession();
+    if (session === null) {
+      throw new Error('No session exist');
+    }
+    const { data: { user }} =
+      await this.db.supabase.auth.getUser(session.access_token)
+    return user;
+  }
+
+  public async getSession() {
+    const { data: {session} } =
+      await this.db.supabase.auth.getSession()
+    return session;
+  }
+
+  public async signOut() {
+    const { error } = await this.db.supabase.auth.signOut();
+    if (error !== null) {
+      throw new Error(JSON.stringify(error));
+    }
+    return error;
+  }
+
+}
