68a5f3455a8c3e9ab10422c9096b9b8e659641d8
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-05-09 23:05:51 -0500

    add login and register functionality

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from 'src/app/services/auth.service';
+import { SessionManagerService } from 'src/app/services/session-manager.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  public fb = inject(FormBuilder);
+  private auth = inject(AuthService);
+  private session = inject(SessionManagerService);
+  public form = signal<FormGroup>(this.fb.group({
+    email: [null, [Validators.required, Validators.email]],
+    password: [null, Validators.required]
+  }))
 
+  public  async login(): Promise<void> {
+    const { email, password } = this.form().value;
+    this.auth.logIn({email, password})
+  }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  public fb = inject(FormBuilder);
+  public auth = inject(AuthService);
+  public form = signal<FormGroup>(this.fb.group({
+    email: [null, [Validators.required, Validators.email]],
+    password: [null, [Validators.required]],
+    check_password: [null, [Validators.required]]
+  }));
 
+  get emailControl() {
+    return this.form().get('email')
+  }
+  get passwordControl() {
+    return this.form().get('password')
+  }
+  get checkPasswordControl() {
+    return this.form().get('check_password')
+  }
+
+  public async createUser(): Promise<void> {
+    const { email, password } = this.form().value;
+    await this.auth.signUp({ email, password })
+  }
 }
