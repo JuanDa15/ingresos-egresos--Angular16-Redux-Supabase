e55ed5ea4e29852feed8dff5d8b70c68ea5bffbe
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-05-09 19:52:28 -0500

    project creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
+import { MainComponent } from './ingreso-egreso/main/main.component';
+import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'auth',
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+    ]
+  },
+  {
+      path: '',
+      component: DashboardComponent,
+      children: [
+        { path: '', component: EstadisticaComponent },
+        { path: 'ingreso-egreso', component: MainComponent },
+        { path: 'detalle', component: DetalleComponent },
+
+      ]
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/ingreso-egreso/ingreso-egreso.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DetalleComponent } from './detalle/detalle.component';
+import { EstadisticaComponent } from './estadistica/estadistica.component';
+import { MainComponent } from './main/main.component';
+
+
+
+@NgModule({
+  declarations: [
+    DetalleComponent,
+    EstadisticaComponent,
+    MainComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class IngresoEgresoModule { }
