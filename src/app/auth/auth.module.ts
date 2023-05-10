eac808997729cf1703d5b193d9bc17c89d5e56c6
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-05-09 23:03:51 -0500

    add reactive forms module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
