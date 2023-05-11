434900c8145bfa00fcaf01515df9c9c80bc3472e
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-05-10 20:29:38 -0500

    add notifications

--- src/app/services/session-manager.service.ts
+++ src/app/services/session-manager.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { SupabaseService } from './supabase.service';
+import { NotificationsService } from './notifications.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,12 @@ import { SupabaseService } from './supabase.service';
 export class SessionManagerService {
 
   public db = inject(SupabaseService);
+  public notification = inject(NotificationsService)
 
   public async getUser() {
     const session = await this.getSession();
     if (session === null) {
+      this.notification.error('No session exist')
       throw new Error('No session exist');
     }
     const { data: { user }} = await this.db.supabase.auth.getUser(session.access_token)
@@ -26,6 +29,7 @@ export class SessionManagerService {
   public async signOut() {
     const { error } = await this.db.supabase.auth.signOut();
     if (error !== null) {
+      this.notification.error(error.message)
       throw new Error(JSON.stringify(error));
     }
     return error;
@@ -34,6 +38,7 @@ export class SessionManagerService {
   public async initializeSession() {
     const { error } = await this.db.supabase.auth.initialize();
     if (error !== null) {
+      this.notification.error(error.message)
       throw new Error(JSON.stringify(error))
     }
   }
