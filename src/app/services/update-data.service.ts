a589e2f18551057cb6d5e72ca2d9cf5e4e648d7e
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-05-10 23:29:54 -0500

    add service to update the data

---
+++ src/app/services/update-data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, inject } from '@angular/core';
+import { SupabaseService } from './supabase.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UpdateDataService {
+  public bd = inject(SupabaseService);
+
+  async updateData(names: string, last_names: string, id: string) {
+    return this.bd.supabase.from('user_information')
+      .update({ names, last_names, updated: true })
+      .eq('uid', id).throwOnError();
+  }
+}
