d229b48c4f847d80c60eba574e2f347a70cb6a3a
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-05-10 23:29:09 -0500

    add update data guard

---
+++ src/app/guards/updated-data.guard.ts
@@ -0,0 +1,11 @@
+import { inject } from '@angular/core';
+import { CanDeactivateFn } from '@angular/router';
+import { SessionManagerService } from '../services/session-manager.service';
+
+export const updatedDataGuard: CanDeactivateFn<unknown> = async () => {
+  const session = inject(SessionManagerService);
+  const currentSession = await session.getSession();
+  const hasUpdatedData = await session.hasUpdatedData();
+  console.log(currentSession);
+  return hasUpdatedData || (currentSession === null);
+};
